tq-systems/meta-tq

commit 826779f84a5c68a0c615a194e7d1dc98e8b745e2

meta-tq: qoriq-atf: add support for tqmls10xxa machines
In contrast to chassis 3 qoriq modules (TQMLS1028A or TQMLX2160a) chassis 2
qoriq modules (TQMLS1043a and TQMLS1046a) have the bootsource hardcoded in
the rcw file. So when generating the different variants we do not have to
consider configurations where the mode of the rcw and the bootmode do not match.

When the rcw filename contains a bootsource build BL2 PBL for this bootsource
only.

Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>

meta-tq/dynamic-layers/freescale-layer/recipes-bsp/atf/qoriq-atf_2.4.bbappend
@@ -12,6 +12,14 @@ RCW_SUFFIX:tqmls1028a = "${@bb.utils.contains('DISTRO_FEATURES', 'secure', '_sbe
 PLATFORM:tqmls1028a = "tqmls1028a_1gb"
 PLATFORM_ADDITIONAL_TARGET:tqmls1028a = "tqmls1028a_4gb"
 
+RCW_FOLDER:tqmls1043a = "tqmls1043a"
+PLATFORM:tqmls1043a = "tqmls1043a"
+PLATFORM_ADDITIONAL_TARGET:tqmls1043a = "tqmls1043a_2gb"
+
+RCW_FOLDER:tqmls1046a = "tqmls1046a"
+PLATFORM:tqmls1046a = "tqmls1046a"
+PLATFORM_ADDITIONAL_TARGET:tqmls1046a = "tqmls1046a_8gb"
+
 RCW_FOLDER:tqmlx2160a = "tqmlx2160a"
 RCW_SUFFIX:tqmlx2160a = "${@bb.utils.contains('DISTRO_FEATURES', 'secure', '_sben.bin', '.bin', d)}"
 PLATFORM:tqmlx2160a = "tqmlx2160a"
@@ -26,7 +34,22 @@ do_compile:prepend () {
 do_compile:append () {
     for plat in ${PLATFORM} ${PLATFORM_ADDITIONAL_TARGET}; do
         for rcw_file in ${ATF_RCW_VARIANTS}; do
+            case $rcw_file in
+                *_sd)
+                rcw_bootmode="sd"
+                ;;
+                *_qspi)
+                rcw_bootmode="qspi"
+                ;;
+                *)
+                rcw_bootmode=""
+                ;;
+            esac
+
             for bootmode in ${BOOTTYPE}; do
+                if [ -n "$rcw_bootmode}" ] && [ "${rcw_bootmode}" != "${bootmode}" ]; then
+	            continue
+                fi
                 make V=1 realclean
                 oe_runmake V=1 pbl PLAT=${plat} BOOT_MODE=${bootmode} RCW=${DEPLOY_DIR_IMAGE}/rcw/${RCW_FOLDER}/${rcw_file}${RCW_SUFFIX}
                 install -d ${S}/atf-variants

commit f58e4b30d1321df8c1ff89213dd71dd41399db28

meta-tq: qoriq-atf: fix variant generation
Due to a typo the condition to skip variant generation was always true.
Fix this to allow variant generation for modules with boot independent rcw.

Fixes: 826779f84a5c ("meta-tq: qoriq-atf: add support for tqmls10xxa machines")
Signed-off-by: Gregor Herburger <gregor.herburger@tq-group.com>

meta-tq/dynamic-layers/freescale-layer/recipes-bsp/atf/qoriq-atf_2.4.bbappend
@@ -51,7 +51,7 @@ do_compile:append () {
             esac
 
             for bootmode in ${BOOTTYPE}; do
-                if [ -n "$rcw_bootmode}" ] && [ "${rcw_bootmode}" != "${bootmode}" ]; then
+                if [ -n "${rcw_bootmode}" ] && [ "${rcw_bootmode}" != "${bootmode}" ]; then
 	            continue
                 fi
                 make V=1 realclean