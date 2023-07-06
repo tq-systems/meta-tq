tq-systems/meta-tq

commit 535e9262146b166fafaada9e918e314abd4fba41

rzg2: append trusted firmware recipe for TQMaRZG2x modules
Signed-off-by: Paul Gerber <Paul.Gerber@tq-group.com>

dynamic-layers/rzg2/recipes-common/recipes-bsp/trusted-firmware-a/trusted-firmware-a.bbappend
@@ -1,0 +1,17 @@
+BRANCH = "TQMaRZG2x-v2.7"
+
+SRC_URI_remove = " \
+	git://github.com/renesas-rz/rzg_trusted-firmware-a.git;branch=${BRANCH};protocol=https \
+"
+
+SRC_URI_append = " \
+	${TQ_GIT_BASEURL}/atf.git;branch=${BRANCH};protocol=${TMP_GIT_PROTOCOL} \
+"
+
+SRCREV = "4d7d8e4778fa54b5b3e997646b177bc00fa91196"
+COMPATIBLE_MACHINE_rzg2h .= "|tqmarzg2x"
+
+# TQMaRZG2M has only one DRAM chip places, so only one channel is used
+ATFW_OPT_remove_tqmarzg2m = "RCAR_DRAM_SPLIT=2"
+
+ATFW_OPT_prepend_tqmarzg2x = "RZG_TQMARZG2X=1 RCAR_SA6_TYPE=1 "

commit 98df6c337bc4c374a592849b6cf03c2cfb5a41a9

rzg2: trusted-firmware-a: fix protocol variable
Fixes: 535e9262146b ("rzg2: append trusted firmware recipe for TQMaRZG2x modules")
Signed-off-by: Paul Gerber <Paul.Gerber@tq-group.com>

dynamic-layers/rzg2/recipes-common/recipes-bsp/trusted-firmware-a/trusted-firmware-a.bbappend
@@ -5,7 +5,7 @@ SRC_URI_remove = " \
 "
 
 SRC_URI_append = " \
-	${TQ_GIT_BASEURL}/atf.git;branch=${BRANCH};protocol=${TMP_GIT_PROTOCOL} \
+	${TQ_GIT_BASEURL}/atf.git;branch=${BRANCH};protocol=${TQ_GIT_PROTOCOL} \
 "
 
 SRCREV = "4d7d8e4778fa54b5b3e997646b177bc00fa91196"