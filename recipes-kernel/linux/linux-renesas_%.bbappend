tq-systems/meta-tq

commit 0191975b01b84ce37779fcc031115f9f641ac876

recipes-kernel: append kernel recipe for TQMaRZG2x modules
Signed-off-by: Paul Gerber <Paul.Gerber@tq-group.com>

recipes-kernel/linux/linux-renesas_%.bbappend
@@ -1,0 +1,14 @@
+SRC_URI_remove = "${KERNEL_URL};protocol=https;nocheckout=1;branch=${BRANCH}"
+
+KERNEL_URL = "${TQ_GIT_BASEURL}/linux-tqmaxx.git"
+BRANCH = "TQMaRZG2x-linux-v5.10.x-cip"
+SRCREV = "2dd3207b997c0e972a28f15043a6b931473f0592"
+
+FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
+
+SRC_URI_append = " \
+    ${KERNEL_URL};protocol=${TQ_GIT_PROTOCOL};nocheckout=1;branch=${BRANCH} \
+    file://tq-generic-devices.cfg \
+"
+
+COMPATIBLE_MACHINE_tqmarzg2x = "tqmarzg2x"

recipes-kernel/linux/linux-renesas/tqmarzg2x/tq-generic-devices.cfg
@@ -1,0 +1,9 @@
+CONFIG_GPIO_PCA953X=y
+CONFIG_GPIO_PCA953X_IRQ=y
+CONFIG_SENSORS_GPIO_FAN=m
+CONFIG_SENSORS_PWM_FAN=m
+CONFIG_SENSORS_JC42=y
+CONFIG_SENSORS_LM75=m
+CONFIG_SENSORS_TMP102=y
+CONFIG_EEPROM_AT24=y
+CONFIG_DP83867_PHY=y

commit 6d426906ad4ea1a86b87d04a6a93058de3cc62ed

linux-renesas: fix removal of Renesas URL in SRC_URI
Bitbake does `remove` after `append`, so the new TQ URL in  SRC_URI
is directly removed if protocol `https` is used.

Fixes: 0191975b01b8 ("recipes-kernel: append kernel recipe for TQMaRZG2x modules")
Signed-off-by: Paul Gerber <Paul.Gerber@tq-group.com>

recipes-kernel/linux/linux-renesas_%.bbappend
@@ -1,4 +1,4 @@
-SRC_URI_remove = "${KERNEL_URL};protocol=https;nocheckout=1;branch=${BRANCH}"
+SRC_URI_remove = "git://github.com/renesas-rz/rz_linux-cip.git;protocol=https;nocheckout=1;branch=${BRANCH}"
 
 KERNEL_URL = "${TQ_GIT_BASEURL}/linux-tqmaxx.git"
 BRANCH = "TQMaRZG2x-linux-v5.10.x-cip"