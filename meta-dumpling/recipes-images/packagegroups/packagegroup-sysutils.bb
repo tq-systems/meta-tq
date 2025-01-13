tq-systems/meta-tq

commit b2aa0202f7e43f90a864dc1c02902116db2e13f3

meta-dumpling: Don't reference u-boot-fw-utils
This has been superseeded by libubootenv.
Also install libubootenv only if virtual/bootloader is set to 'u-boot*'

Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

meta-dumpling/recipes-images/packagegroups/packagegroup-sysutils.bb
@@ -5,7 +5,7 @@ inherit packagegroup
 
 RDEPENDS:${PN} = "\
     cpufrequtils \
+    ${@oe.utils.ifelse(d.getVar('PREFERRED_PROVIDER_virtual/bootloader').startswith('u-boot'), 'libubootenv', '')} \
     linuxptp \
     rng-tools \
-    u-boot-fw-utils \
 "

commit a5b69f71393a36456b6fec2392f16f6285a039dd

meta-dumpling: packagegroup-sysutils: conditionally install libubootenv-bin
RDEPEND on libubootenv will trigger install of the library. To install the
example binary libubootenv-bin has to be added to RDEPENDS.

Fixes: b2aa0202f7e4 ("meta-dumpling: Don't reference u-boot-fw-utils")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

meta-dumpling/recipes-images/packagegroups/packagegroup-sysutils.bb
@@ -8,7 +8,7 @@ inherit packagegroup
 
 RDEPENDS:${PN} = "\
     cpufrequtils \
-    ${@oe.utils.ifelse(d.getVar('PREFERRED_PROVIDER_virtual/bootloader').startswith('u-boot'), 'libubootenv', '')} \
+    ${@oe.utils.ifelse(d.getVar('PREFERRED_PROVIDER_virtual/bootloader').startswith('u-boot'), 'libubootenv-bin', '')} \
     linuxptp \
     procps-sysctl \
     rng-tools \