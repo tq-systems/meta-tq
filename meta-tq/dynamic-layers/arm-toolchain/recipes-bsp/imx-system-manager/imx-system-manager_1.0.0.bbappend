tq-systems/meta-tq

commit a1ad5a96afc451977a3915083e2a455e57e3605c

meta-tq: imx-system-manager: add TQ-specific .bbappend
This updates imx-system-manager to 71f528b1ac59bd53351a439e31f969ad1aed4128
by using TQ fork and a new branch 'TQM-lf-6.6.52'

Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

meta-tq/dynamic-layers/arm-toolchain/recipes-bsp/imx-system-manager/imx-system-manager_1.0.0.bbappend
@@ -1,0 +1,10 @@
+SUMMARY = "i.MX System Manager Firmware for TQ modules"
+
+LIC_FILES_CHKSUM = "file://LICENSE.txt;md5=b66f32a90f9577a5a3255c21d79bc619"
+
+IMX_SYSTEM_MANAGER_SRC = "${TQ_GIT_BASEURL}/tq-imx-sm.git;protocol=https"
+SRCBRANCH = "TQM-lf-6.6.52"
+SRCREV = "71f528b1ac59bd53351a439e31f969ad1aed4128"
+
+COMPATIBLE_MACHINE = "^$"
+COMPATIBLE_MACHINE:tqma95xx = "tqma95xx"

commit b8a20a4b622270b0c00cea62db0fea9e8dc36833

meta-tq: dynamic-layers: move and rewrite imx-system-manager
The solution proposed in meta-freescale has some drawbacks:

- When referencing the include to write an own recipe a full path
  must be given since the include is under dynamic layers in
  meta-freescale
- doing a bbappend here under dynamic layer for arm-toolchain may
  cause build errors, when arm-toolchain is present but meta-freescale
  where the recipe comes from is not

The proposed fix ist to add a check for arm-toolchain to the generic
include and raise a bb.parse.SkipPackage exception if not. Since
the recipe is only processed if in dependency chain or directly
addressed no build error is expected under normal conditions.

Fixes: a1ad5a96afc4 ("meta-tq: imx-system-manager: add TQ-specific .bbappend")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

meta-tq/dynamic-layers/arm-toolchain/recipes-bsp/imx-system-manager/imx-system-manager_1.0.0.bbappend
@@ -1,13 +1,0 @@
-SUMMARY = "i.MX System Manager Firmware for TQ modules"
-
-LIC_FILES_CHKSUM = "file://LICENSE.txt;md5=b66f32a90f9577a5a3255c21d79bc619"
-
-IMX_SYSTEM_MANAGER_SRC = "${TQ_GIT_BASEURL}/tq-imx-sm.git;protocol=https"
-SRCBRANCH = "TQM-lf-6.6.52"
-SRCREV = "71f528b1ac59bd53351a439e31f969ad1aed4128"
-
-# needs to be removed for production releases
-PACKAGECONFIG ?= "m1"
-
-COMPATIBLE_MACHINE = "^$"
-COMPATIBLE_MACHINE:tqma95xx = "tqma95xx"

meta-tq/dynamic-layers/freescale-layer/recipes-bsp/imx-system-manager/imx-system-manager-tq_1.0.0.bb
@@ -1,0 +1,33 @@
+SUMMARY = "i.MX System Manager Firmware for TQ modules"
+DESCRIPTION = "\
+The System Manager (SM) is a firmware that runs on a Cortex-M processor on \
+many NXP i.MX processors. The Cortex-M is the boot core, runs the boot ROM \
+which loads the SM (and other boot code), and then branches to the SM. The \
+SM then configures some aspects of the hardware such as isolation mechanisms \
+and then starts other cores in the system. After starting these cores, it \
+enters a service mode where it provides access to clocking, power, sensor, \
+and pin control via a client RPC API based on ARM's System Control and \
+Management Interface (SCMI). This recipe provides the implementation for \
+TQ-Systems SOM"
+LICENSE = "BSD-3-Clause"
+LIC_FILES_CHKSUM = "file://LICENSE.txt;md5=b66f32a90f9577a5a3255c21d79bc619"
+
+SRC_URI = "${IMX_SYSTEM_MANAGER_SRC};branch=${SRCBRANCH}"
+IMX_SYSTEM_MANAGER_SRC = "${TQ_GIT_BASEURL}/tq-imx-sm.git;protocol=${TQ_GIT_PROTOCOL}"
+SRCBRANCH = "TQM-lf-6.6.52"
+SRCREV = "71f528b1ac59bd53351a439e31f969ad1aed4128"
+
+S = "${WORKDIR}/git"
+
+# TODO: use full path after patches are ported to meta-freescale
+require imx-system-manager.inc
+# require recipes-bsp/imx-system-manager/imx-system-manager.inc
+
+# needs to be removed for production releases
+PACKAGECONFIG ?= "m1"
+
+# should be the output file name w/o extension
+SYSTEM_MANAGER_FIRMWARE_BASENAME ?= "m33_image"
+
+COMPATIBLE_MACHINE = "^$"
+COMPATIBLE_MACHINE:tqma95xx = "tqma95xx"

meta-tq/dynamic-layers/freescale-layer/recipes-bsp/imx-system-manager/imx-system-manager.inc
@@ -1,0 +1,53 @@
+
+python () {
+    layers = d.getVar("BBFILE_COLLECTIONS")
+    arm_toolchain_collection = "arm-toolchain"
+
+    if not arm_toolchain_collection in layers:
+        raise bb.parse.SkipRecipe("Building %s requires %s\n" % (d.getVar('PN'), arm_toolchain_collection))
+}
+
+# Set generic compiler for system manager core
+INHIBIT_DEFAULT_DEPS = "1"
+DEPENDS = "${SM_COMPILER}"
+SM_COMPILER ?= "gcc-arm-none-eabi-native"
+PROVIDES += "virtual/imx-system-manager"
+
+inherit deploy
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"
+
+# Set monitor mode for none, one, or two
+PACKAGECONFIG[m0] = "M=0,,,,,m1 m2"
+PACKAGECONFIG[m1] = ",,,,,m0 m2"
+PACKAGECONFIG[m2] = "M=2,,,,,m0 m1"
+
+SYSTEM_MANAGER_CONFIG ?= "INVALID"
+
+LDFLAGS[unexport] = "1"
+
+EXTRA_OEMAKE = " \
+    V=y \
+    SM_CROSS_COMPILE=arm-none-eabi- \
+    ${PACKAGECONFIG_CONFARGS} \
+"
+
+do_configure() {
+    oe_runmake config=${SYSTEM_MANAGER_CONFIG} clean
+    oe_runmake config=${SYSTEM_MANAGER_CONFIG} cfg
+}
+
+do_compile() {
+    oe_runmake config=${SYSTEM_MANAGER_CONFIG}
+}
+
+do_install[noexec] = "1"
+
+addtask deploy after do_compile
+do_deploy() {
+    install -D -p -m 0644 \
+        ${B}/build/${SYSTEM_MANAGER_CONFIG}/${SYSTEM_MANAGER_FIRMWARE_BASENAME}.bin \
+        ${DEPLOYDIR}/${SYSTEM_MANAGER_FIRMWARE_BASENAME}-${SYSTEM_MANAGER_CONFIG}.bin
+}
+
+COMPATIBLE_MACHINE = "(mx95-generic-bsp)"