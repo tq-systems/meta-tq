tq-systems/meta-tq

commit 50307ec8b66f98e930cb0a93bb5f3ee22022af4b

meta-tq: qoriq-atf: Bump to version 2.10
qoriq-atf_2.10.bb, qoriq-atf-tools_2.10.bb and qoriq-atf_2.10.inc are
taken from meta-qoriq branch styhead with UNPACKDIR replaced by WORKDIR.
qoriq-atf_2.10.bbappend is copied from 2.4 and superfluous settings
removed.

Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

meta-tq/dynamic-layers/freescale-layer/recipes-bsp/atf/qoriq-atf-tools_2.10.bb
@@ -1,0 +1,15 @@
+require qoriq-atf-${PV}.inc
+
+DEPENDS += "openssl"
+
+PV:append = "+${SRCPV}"
+
+EXTRA_OEMAKE = "fiptool V=1 PLAT=lx2162aqds HOSTCC='${CC} ${CPPFLAGS} ${CFLAGS} ${LDFLAGS}'"
+
+do_install () {
+    install -m 0755 -d ${D}/${bindir}
+    install -m 0755 ${S}/tools/fiptool/fiptool ${D}/${bindir}/
+}
+
+BBCLASSEXTEND = "native"
+

meta-tq/dynamic-layers/freescale-layer/recipes-bsp/atf/qoriq-atf_2.10.bb
@@ -1,14 +1,17 @@
-require qoriq-atf-2.4.inc
+require qoriq-atf-${PV}.inc
 
 inherit deploy
 
-DEPENDS += "u-boot-mkimage-native u-boot openssl openssl-native rcw qoriq-cst-native bc-native"
-DEPENDS:append:lx2160a = " ddr-phy"
-DEPENDS:append:lx2162a = " ddr-phy"
+DEPENDS += "u-boot-mkimage-native u-boot openssl openssl-native mbedtls rcw qoriq-cst-native bc-native"
 do_compile[depends] += "u-boot:do_deploy rcw:do_deploy uefi:do_deploy"
 
-SRC_URI += "git://github.com/ARMmbed/mbedtls;nobranch=1;destsuffix=git/mbedtls;name=mbedtls;protocol=https"
+PV:append = "+${SRCPV}"
+
+SRC_URI += "git://github.com/ARMmbed/mbedtls;protocol=https;nobranch=1;destsuffix=git/mbedtls;name=mbedtls \
+    git://github.com/nxp/ddr-phy-binary;protocol=https;nobranch=1;destsuffix=git/ddr-phy-binary;name=ddr \
+"
 SRCREV_mbedtls = "0795874acdf887290b2571b193cafd3c4041a708"
+SRCREV_ddr = "fbc036b88acb6c06ffed02c898cbae9856ec75ba"
 SRCREV_FORMAT = "atf"
 
 COMPATIBLE_MACHINE = "(qoriq)"
@@ -17,12 +20,14 @@ PACKAGE_ARCH = "${MACHINE_ARCH}"
 
 PLATFORM = "${MACHINE}"
 PLATFORM:ls1088ardb-pb = "ls1088ardb"
+PLATFORM:lx2160ardb-rev2 = "lx2160ardb"
 PLATFORM_ADDITIONAL_TARGET ??= ""
 PLATFORM_ADDITIONAL_TARGET:ls1012afrwy = "ls1012afrwy_512mb"
 
 RCW_FOLDER ?= "${MACHINE}"
 RCW_FOLDER:ls1088ardb-pb = "ls1088ardb"
 RCW_FOLDER:lx2160ardb = "lx2160ardb_rev2"
+RCW_FOLDER:lx2160ardb-rev2 = "lx2160ardb_rev2"
 
 RCW_SUFFIX ?= ".bin"
 RCW_SUFFIX:ls1012a = "${@bb.utils.contains('DISTRO_FEATURES', 'secure', '_sben.bin', '_default.bin', d)}"
@@ -40,9 +45,9 @@ chassistype:ls1012a = "ls104x_1012"
 chassistype:ls1043a = "ls104x_1012"
 chassistype:ls1046a = "ls104x_1012"
 
-DDR_PHY_BIN_PATH ?= ""
-DDR_PHY_BIN_PATH:lx2160a = "${DEPLOY_DIR_IMAGE}/ddr-phy"
-DDR_PHY_BIN_PATH:lx2162a = "${DEPLOY_DIR_IMAGE}/ddr-phy"
+FIP_DDR ?= ""
+FIP_DDR:lx2160a = "${@bb.utils.contains('DISTRO_FEATURES', 'secure', 'fip_ddr', '', d)}"
+FIP_DDR:lx2162a = "${@bb.utils.contains('DISTRO_FEATURES', 'secure', 'fip_ddr', '', d)}"
 
 # requires CROSS_COMPILE set by hand as there is no configure script
 export CROSS_COMPILE="${TARGET_PREFIX}"
@@ -125,18 +130,15 @@ do_compile() {
                 cp *.pem build/${PLATFORM}/release/
             fi
 
-            oe_runmake V=1 all fip pbl PLAT=${PLATFORM} BOOT_MODE=${d} RCW=${DEPLOY_DIR_IMAGE}/rcw/${RCW_FOLDER}/${rcwimg} BL33=${UBOOT_BINARY}
+            oe_runmake V=1 all fip pbl ${FIP_DDR} PLAT=${PLATFORM} BOOT_MODE=${d} RCW=${DEPLOY_DIR_IMAGE}/rcw/${RCW_FOLDER}/${rcwimg} BL33=${UBOOT_BINARY}
             cp build/${PLATFORM}/release/bl2_${d}${SECURE_EXTENTION}.pbl .
             cp build/${PLATFORM}/release/fip.bin fip_uboot${SECURE_EXTENTION}.bin
             if [ -e build/${PLATFORM}/release/fuse_fip.bin ]; then
                 cp build/${PLATFORM}/release/fuse_fip.bin .
             fi
 
-            if [ -n "${SECURE_EXTENTION}" -a -n "${DDR_PHY_BIN_PATH}" -a ! -f ddr_fip_sec.bin ]; then
-                oe_runmake V=1 fip_ddr PLAT=${PLATFORM} DDR_PHY_BIN_PATH=${DDR_PHY_BIN_PATH}
-                if [ -e build/${PLATFORM}/release/ddr_fip_sec.bin ]; then
-                    cp build/${PLATFORM}/release/ddr_fip_sec.bin .
-                fi
+            if [ -e build/${PLATFORM}/release/ddr_fip_sec.bin ] && [ ! -f ddr_fip_sec.bin ]; then
+                cp build/${PLATFORM}/release/ddr_fip_sec.bin .
             fi
 
             if [ -e build/${PLATFORM}/release/rot_key.pem ] && [ ! -f rot_key.pem ]; then

meta-tq/conf/machine/include/tqmls1012al.inc
@@ -60,7 +60,7 @@ RCWQSPI ?= "default/rcw_1000_sd"
 
 RCW_FOLDER:${MACHINE} = "tqmls1012al"
 RCW_SUFFIX:${MACHINE} = "${@bb.utils.contains('DISTRO_FEATURES', 'secure', '_sben.bin', '.bin', d)}"
-PLATFORM:${MACHINE} = "tqmls1012al"
+PLATFORM:${MACHINE} = "tqmls1012al_512mb"
 PLATFORM_ADDITIONAL_TARGET:${MACHINE} = "tqmls1012al_1gb"
 
 # Common image configuration

meta-tq/conf/machine/include/tqmlx2160a.inc
@@ -63,7 +63,7 @@ BL2_IMAGE ?= "bl2_auto.pbl"
 BL3_IMAGE ?= "fip_uboot.bin"
 
 RCW_FOLDER:${MACHINE} = "tqmlx2160a"
-PLATFORM:${MACHINE} = "tqmlx2160a"
+PLATFORM:${MACHINE} = "tqmlx2160a_32gb"
 PLATFORM_ADDITIONAL_TARGET:${MACHINE} = "tqmlx2160a_16gb"
 
 # Common image configuration

meta-tq/conf/machine/tqmls1043a-1gb-mbls10xxa.conf
@@ -24,4 +24,4 @@
 
 require conf/machine/include/tqmls1043a-mbls10xxa.inc
 
-TQ_ATF_VARIANT = "tqmls1043a"
+TQ_ATF_VARIANT = "tqmls1043a_1gb"

meta-tq/conf/machine/tqmls1043a-2gb-rev0300-mbls10xxa.conf
@@ -24,4 +24,4 @@
 
 require conf/machine/include/tqmls1043a-mbls10xxa.inc
 
-TQ_ATF_VARIANT = "tqmls1043a_2gb_rev0300"
+TQ_ATF_VARIANT = "tqmls1043a_rev300_2gb"

meta-tq/conf/machine/tqmls1046a-2gb-mbls10xxa.conf
@@ -24,4 +24,4 @@
 
 require conf/machine/include/tqmls1046a-mbls10xxa.inc
 
-TQ_ATF_VARIANT = "tqmls1046a"
+TQ_ATF_VARIANT = "tqmls1046a_2gb"

meta-tq/conf/machine/tqmls1088a-2gb-mbls10xxa.conf
@@ -15,4 +15,4 @@
 
 require conf/machine/include/tqmls1088a-mbls10xxa.inc
 
-TQ_ATF_VARIANT = "tqmls1088a"
+TQ_ATF_VARIANT = "tqmls1088a_2gb"

commit 511a1dbf72fbdecf609e4cb1e0be807f3ff15776

meta-tq: dynamic-layers/freescale-layer: qoriq-atf[-tools]: do not use SRCPV
This variable was deprecated long time ago and should not be used.

Fixes: 50307ec8b66f ("meta-tq: qoriq-atf: Bump to version 2.10")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

meta-tq/dynamic-layers/freescale-layer/recipes-bsp/atf/qoriq-atf-tools_2.10.bb
@@ -2,8 +2,6 @@ require qoriq-atf-${PV}.inc
 
 DEPENDS += "openssl"
 
-PV:append = "+${SRCPV}"
-
 EXTRA_OEMAKE = "fiptool V=1 PLAT=lx2162aqds HOSTCC='${CC} ${CPPFLAGS} ${CFLAGS} ${LDFLAGS}'"
 
 do_install () {
@@ -12,4 +10,3 @@ do_install () {
 }
 
 BBCLASSEXTEND = "native"
-

meta-tq/dynamic-layers/freescale-layer/recipes-bsp/atf/qoriq-atf_2.10.bb
@@ -5,8 +5,6 @@ inherit deploy
 DEPENDS += "u-boot-mkimage-native u-boot openssl openssl-native mbedtls rcw qoriq-cst-native bc-native"
 do_compile[depends] += "u-boot:do_deploy rcw:do_deploy uefi:do_deploy"
 
-PV:append = "+${SRCPV}"
-
 SRC_URI += "git://github.com/ARMmbed/mbedtls;protocol=https;nobranch=1;destsuffix=git/mbedtls;name=mbedtls \
     git://github.com/nxp/ddr-phy-binary;protocol=https;nobranch=1;destsuffix=git/ddr-phy-binary;name=ddr \
 "