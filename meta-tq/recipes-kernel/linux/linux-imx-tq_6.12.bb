tq-systems/meta-tq

commit c3f6bd47ed89fbd533be11454f4564f0f56b9d6e

meta-tq: linux-imx-tq_6.12: add missing configs
Port feature includes from other recipes to get missing drivers / modules:

- iio: IIO hwmon virtual device support and MCP320X fan controller
- pwm: IMX_TPM
- spi: SPIDEV (spi userspace device)
- ubi: MTD_PARTITIONED_MASTER, MTD_UBI, UBIFS_FS (rootfs on UBI)

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

meta-tq/recipes-kernel/linux/linux-imx-tq-6.12/features/pwm.cfg
@@ -1,0 +1,1 @@
+CONFIG_PWM_IMX_TPM=m

meta-tq/recipes-kernel/linux/linux-imx-tq_6.12.bb
@@ -35,9 +35,13 @@ SRC_URI:aarch64 = "\
     file://enable-led-features.cfg \
     file://features/debug.cfg \
     file://features/devicetree.cfg \
+    file://features/iio.cfg \
     file://features/network.cfg \
+    file://features/pwm.cfg \
     file://features/rauc.cfg \
+    file://features/spidev.cfg \
     file://features/trusted-keys.cfg \
+    file://features/ubi.cfg \
     file://features/usb.cfg \
     file://gpio-enablement.cfg \
     file://imx8m-platform-support.cfg \

meta-tq/recipes-kernel/linux/linux-imx-tq-6.12/features/iio.cfg
@@ -1,0 +1,2 @@
+CONFIG_SENSORS_IIO_HWMON=y
+CONFIG_MCP320X=y

meta-tq/recipes-kernel/linux/linux-imx-tq-6.12/features/spidev.cfg
@@ -1,0 +1,1 @@
+CONFIG_SPI_SPIDEV=m

meta-tq/recipes-kernel/linux/linux-imx-tq-6.12/features/ubi.cfg
@@ -1,0 +1,5 @@
+CONFIG_MTD_PARTITIONED_MASTER=y
+# CONFIG_MTD_SPI_NOR_USE_4K_SECTORS is not set
+CONFIG_MTD_UBI=y
+CONFIG_UBIFS_FS=y
+# CONFIG_MTD_UBI_FASTMAP is not set

commit 9294538bfc9c26feb42ea2f8664bba398f209b00

meta-tq: linux-imx-tq-6.12: fix PWM_IMX_TPM handling
When porting missing config fragments an error was introduced for
PWM_IMX_TPM. Remove imx93-platform-support.cfg as the driver is
needed for i.MX95 too and use 'y' instead of 'm' since fslc/imx
downstream graphic stack has problems with modules and deferred
probe.

Fixes: c3f6bd47ed89 ("meta-tq: linux-imx-tq_6.12: add missing configs")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

meta-tq/recipes-kernel/linux/linux-imx-tq-6.12/features/pwm.cfg
@@ -1,1 +1,1 @@
-CONFIG_PWM_IMX_TPM=m
+CONFIG_PWM_IMX_TPM=y

meta-tq/recipes-kernel/linux/linux-imx-tq-6.12/imx-arm64/imx93-platform-support.cfg
@@ -1,1 +1,0 @@
-CONFIG_PWM_IMX_TPM=y

meta-tq/recipes-kernel/linux/linux-imx-tq_6.12.bb
@@ -45,7 +45,6 @@ SRC_URI:aarch64 = "\
     file://features/usb.cfg \
     file://gpio-enablement.cfg \
     file://imx8m-platform-support.cfg \
-    file://imx93-platform-support.cfg \
     file://input-devices.cfg \
     file://local-version.cfg \
     file://network-support.cfg \