tq-systems/meta-tq

commit 5301a425450161643e1732c3c5918659347ed94a

meta-tq: linux-imx-tq_6.12: update to b76fe99715e14aea8eb5165a0443b60f69990b1c
b76fe99715e14 Merge remote-tracking branch 'fslc/6.12-2.0.x-imx' into TQMa-fslc-6.12-2.0.x-imx
e1f7a3f59ae98 Merge remote-tracking branch '6.12-2.0.x-imx' into TQMa-fslc-6.12-2.0.x-imx
8ef154d2c3a3c usb: chipidea: ci_hdrc_imx: fix ci_hdrc_imx_probe error path
2ce53a50fbf85 net: enetc: disable serdes regulator if enetc_pci_mdio_probe fails
a83228624c481 net: enetc: disable serdes regulator if enetc_imdio_create fails
3224660c220fd nvmem: [HACK] Kconfig: depend upstream edgelock nvmem on !NVMEM_IMX_OCOTP_FSB_S400

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

meta-tq/recipes-kernel/linux/linux-imx-tq_6.12.bb
@@ -10,11 +10,11 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=6bc538ed5bd9a7fc9398086aedcd7e46"
 require linux-imx-tq-common.inc
 
 KBRANCH = "TQMa-fslc-6.12-2.0.x-imx"
-SRCREV = "ab49f08ca6fa437123a480ab1aaa34c59f03d0b1"
+SRCREV = "b76fe99715e14aea8eb5165a0443b60f69990b1c"
 
 # LINUX_VERSION must match version from Makefile
 LINUX_RELEASE = "6.12"
-LINUX_VERSION = "${LINUX_RELEASE}.49"
+LINUX_VERSION = "${LINUX_RELEASE}.55"
 
 FILESEXTRAPATHS:prepend := "${THISDIR}/${PN}-${LINUX_RELEASE}:"
 FILESEXTRAPATHS:prepend:aarch64 := "${THISDIR}/${PN}-${LINUX_RELEASE}/imx-arm64:"

commit 3cb2f2fbce0565742f0732aa8c5952e726389926

meta-tq: linux-imx-tq_6.12: enable downstream edgelock NVMEM
The downstream kernel has two drivers for edgelock NVME. The fix that was
implemented in the kernel branch used by this recipe requires,
that the correct driver is selected explicitely in the kernel config.

Fixes: 5301a4254501 ("meta-tq: linux-imx-tq_6.12: update to b76fe99715e14aea8eb5165a0443b60f69990b1c")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

meta-tq/recipes-kernel/linux/linux-imx-tq-6.12/imx-arm64/imx9-platform-support.cfg
@@ -1,0 +1,1 @@
+CONFIG_NVMEM_IMX_OCOTP_FSB_S400=y

meta-tq/recipes-kernel/linux/linux-imx-tq_6.12.bb
@@ -45,6 +45,7 @@ SRC_URI:aarch64 = "\
     file://features/usb.cfg \
     file://gpio-enablement.cfg \
     file://imx8m-platform-support.cfg \
+    file://imx9-platform-support.cfg \
     file://input-devices.cfg \
     file://local-version.cfg \
     file://network-support.cfg \