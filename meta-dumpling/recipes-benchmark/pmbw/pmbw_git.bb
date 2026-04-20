tq-systems/meta-tq

commit 6bfdadf4d4415f9866af8c7807f8aafccbdff109

meta-dumpling: add pmbw benchmark
This tool uses a set of assembler routines to measure the parallel memory
(cache and RAM) bandwidth of modern multi-core machines.

Recipe was ported from https://github.com/phytec/meta-yogurt
branch thud, updated to 0.6.3 and adjusted to newer yocto versions.

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

meta-dumpling/recipes-benchmark/pmbw/pmbw_git.bb
@@ -1,0 +1,31 @@
+# SPDX-License-Identifier: MIT
+# based on recipe from https://github.com/phytec/meta-yogurt, branch thud
+
+SUMMARY = "Parallel Memory Bandwidth Measurement / Benchmark"
+DESCRIPTION = "\
+The tool pmbw is a set of assembler routines to measure the parallel memory \
+(cache and RAM) bandwidth of modern multi-core machines."
+HOMEPAGE = "http://panthema.net/2013/pmbw/"
+SECTION = "benchmark/tests"
+AUTHOR = "Timo Bingmann"
+LICENSE = "GPL-3.0-or-later"
+LIC_FILES_CHKSUM = "file://COPYING;md5=d32239bcb673463ab874e80d47fae504"
+
+SRC_URI = "git://github.com/bingmann/pmbw;protocol=https;branch=master"
+
+PV = "0.6.3+git${SRCPV}"
+SRCREV = "1a2f3ff96e6c9b8f9fc85084909edde6afac34ce"
+
+S = "${WORKDIR}/git"
+
+inherit autotools
+
+# the hand coded asm uses r11, which therefore cannot be used for storing
+# the frame pointer when debugging on arm
+SELECTED_OPTIMIZATION:remove:arm = "-fno-omit-frame-pointer"
+
+PACKAGES =+ "${PN}-stats2gnuplot"
+
+FILES:${PN}-stats2gnuplot = "${bindir}/stats2gnuplot"
+
+RRECOMMENDS:${PN} = "${PN}-stats2gnuplot"

commit dca6cdec5058a80f3a2b91093ffe17f8d08e53cb

meta-dumpling: pmbw_git: do not use SRCPV
SRCPV variable is no longer needed, using '+' is enough.
Value of SRCPV is "", so using it is a NOP. Remove usage
to make code cleaner and prevent further usage by copies.

See poky commit: 9f966b15f839 ("migration-4.3: additional migration items")

Fixes: 6bfdadf4d441 ("meta-dumpling: add pmbw benchmark")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

meta-dumpling/recipes-benchmark/pmbw/pmbw_git.bb
@@ -13,7 +13,7 @@ LIC_FILES_CHKSUM = "file://COPYING;md5=d32239bcb673463ab874e80d47fae504"
 
 SRC_URI = "git://github.com/bingmann/pmbw;protocol=https;branch=master"
 
-PV = "0.6.3+git${SRCPV}"
+PV = "0.6.3+git"
 SRCREV = "1a2f3ff96e6c9b8f9fc85084909edde6afac34ce"
 
 S = "${WORKDIR}/git"