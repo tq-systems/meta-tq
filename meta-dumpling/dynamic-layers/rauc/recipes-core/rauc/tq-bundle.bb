tq-systems/meta-tq

commit 4c8e7d1ac1ab96e3b25724c9b243ed94fd5fa688

meta-dumpling: add example RAUC configuration
Two system.conf variants are added for boot from eMMC and SD card (the
latter to facilitate testing).

Signed-off-by: Matthias Schiffer <matthias.schiffer@tq-group.com>

meta-dumpling/conf/distro/include/tq-common.inc
@@ -12,6 +12,18 @@ DISTRO_VERSION ??= "${VENDOR_SHORT_NAME}.0.1"
 # vulnerabilities cannot be exploited.
 RAUC_BUNDLE_VERSION ?= "r0"
 
+SKIP_RECIPE[rauc-conf] = "replaced with tq-rauc-conf"
+
+# RAUC example key - located under (and used by recipes in) dynamic-layers/rauc
+RAUC_KEYRING_FILE ?= "example.cert.pem"
+RAUC_CERT_FILE ?= "example.cert.pem"
+RAUC_KEY_FILE ?= "example.key.pem"
+
+# RAUC upgrades are possible between images that use the same
+# `RAUC_BUNDLE_COMPATIBLE`. The reference distros in meta-dumpling always use
+# "dumpling-${MACHINE}", so upgrades between dumpling and spaetzle are supported.
+RAUC_BUNDLE_COMPATIBLE ?= "dumpling-${MACHINE}"
+
 SDK_VENDOR = "-${VENDOR_SHORT_NAME}sdk"
 SDK_VERSION := "${DISTRO_VERSION}"
 SDK_NAME = "${VENDOR_SHORT_NAME}-${DISTRO}-${TCLIBC}-${SDK_ARCH}-${IMAGE_BASENAME}-${TUNE_PKGARCH}"

meta-dumpling/dynamic-layers/rauc/recipes-core/rauc/tq-bundle.bb
@@ -1,0 +1,30 @@
+inherit bundle
+
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
+
+RAUC_KEY_URI ??= "file://${RAUC_KEY_FILE}"
+RAUC_CERT_URI ??= "file://${RAUC_CERT_FILE}"
+RAUC_KEYRING_URI ??= "file://${RAUC_KEYRING_FILE}"
+
+SRC_URI = " \
+    ${RAUC_KEY_URI} \
+    ${RAUC_CERT_URI} \
+    ${RAUC_KEYRING_URI} \
+"
+
+RAUC_BUNDLE_FORMAT = "verity"
+RAUC_BUNDLE_SLOTS = "rootfs"
+
+# Smaller bundles can be achieved using a more modern compression algorithm like XZ or ZSTD
+RAUC_SLOT_rootfs ?= "tq-image-generic-debug"
+RAUC_SLOT_rootfs[fstype] ?= "tar.gz"
+
+do_bundle:prepend () {
+    (
+        cd "${WORKDIR}" && for file in "${RAUC_KEY_FILE}" "${RAUC_CERT_FILE}" "${RAUC_KEYRING_FILE}"; do
+            mkdir -p "$(dirname "${B}/${file}")"
+            cp "${file}" "${B}/${file}"
+        done
+    )
+}

meta-dumpling/dynamic-layers/rauc/recipes-core/rauc/tq-rauc-conf.bb
@@ -1,0 +1,69 @@
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2024 TQ-Systems GmbH <oss@ew.tq-group.com>, D-82229 Seefeld, Germany.
+# Author: Matthias Schiffer
+#
+# Based on rauc-conf.bb from meta-rauc.
+
+SUMMARY = "RAUC system configuration & verification keyring (TQ example config)"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COREBASE}/meta/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+
+RAUC_KEYRING_URI ??= "file://${RAUC_KEYRING_FILE}"
+
+RDEPENDS:${PN} += "util-linux-findmnt"
+RPROVIDES:${PN} += "virtual-rauc-conf"
+
+INHIBIT_DEFAULT_DEPS = "1"
+do_compile[noexec] = "1"
+
+RAUC_SYSTEM_CONF_VARIANTS = "mmc0 mmc1"
+
+SRC_URI = " \
+    ${RAUC_KEYRING_URI} \
+    file://rauc-conf.sh \
+    file://system.conf-mmc0 \
+    file://system.conf-mmc1 \
+    file://tq-rauc-conf.conf \
+    file://tq-rauc-conf.service \
+"
+
+inherit systemd update-rc.d
+
+SYSTEMD_SERVICE:${PN} = "tq-rauc-conf.service"
+
+INITSCRIPT_NAME = "tq-rauc-conf.sh"
+INITSCRIPT_PARAMS = "start 10 S ."
+
+do_install () {
+    install -d "${D}${sysconfdir}/rauc"
+    install -m 0644 "${WORKDIR}/${RAUC_KEYRING_FILE}" "${D}${sysconfdir}/rauc/"
+    ln -s /run/tq/rauc/system.conf "${D}${sysconfdir}/rauc/system.conf"
+
+    for variant in ${RAUC_SYSTEM_CONF_VARIANTS}; do
+        install -m 0644 "${WORKDIR}/system.conf-${variant}" "${D}${sysconfdir}/rauc/"
+        sed -i \
+            -e "s!@RAUC_BUNDLE_COMPATIBLE@!${RAUC_BUNDLE_COMPATIBLE}!g" \
+            -e "s!@RAUC_KEYRING_FILE@!$(basename "${RAUC_KEYRING_FILE}")!g" \
+            "${D}${sysconfdir}/rauc/system.conf-${variant}"
+    done
+
+    install -Dm 0755 "${WORKDIR}/rauc-conf.sh" "${D}${libexecdir}/tq/rauc-conf.sh"
+
+    if ${@bb.utils.contains('DISTRO_FEATURES', 'sysvinit', 'true', 'false', d)}; then
+	    install -d ${D}${sysconfdir}/init.d/
+        ln -s ${libexecdir}/tq/rauc-conf.sh ${D}${sysconfdir}/init.d/tq-rauc-conf.sh
+    fi
+
+    if ${@bb.utils.contains('DISTRO_FEATURES', 'systemd', 'true', 'false', d)}; then
+        install -Dm 0644 "${WORKDIR}/tq-rauc-conf.service" "${D}${systemd_system_unitdir}/tq-rauc-conf.service"
+        sed -i -e "s,@LIBEXECDIR@,${libexecdir},g" "${D}${systemd_system_unitdir}/tq-rauc-conf.service"
+
+        install -Dm 0644 "${WORKDIR}/tq-rauc-conf.conf" "${D}${systemd_system_unitdir}/rauc.service.d/10-tq-rauc-conf.conf"
+        install -Dm 0644 "${WORKDIR}/tq-rauc-conf.conf" "${D}${systemd_system_unitdir}/rauc-mark-good.service.d/10-tq-rauc-conf.conf"
+    fi
+}
+
+FILES:${PN} += "${systemd_system_unitdir}"
+
+PACKAGE_ARCH = "${MACHINE_ARCH}"

meta-dumpling/conf/layer.conf
@@ -19,6 +19,8 @@ BBFILES_DYNAMIC += "\
     freescale-layer:${LAYERDIR}/dynamic-layers/freescale-layer/recipes-*/*/*.bb \
     meta-ti-bsp:${LAYERDIR}/dynamic-layers/meta-ti-bsp/recipes-*/*/*.bbappend \
     meta-ti-bsp:${LAYERDIR}/dynamic-layers/meta-ti-bsp/recipes-*/*/*.bb \
+    rauc:${LAYERDIR}/dynamic-layers/rauc/recipes-*/*/*.bbappend \
+    rauc:${LAYERDIR}/dynamic-layers/rauc/recipes-*/*/*.bb \
     qt5-layer:${LAYERDIR}/dynamic-layers/qt5-layer/recipes-*/*/*.bbappend \
     qt5-layer:${LAYERDIR}/dynamic-layers/qt5-layer/recipes-*/*/*.bb \
 "

meta-dumpling/dynamic-layers/rauc/recipes-core/rauc/files/example.cert.pem
@@ -1,0 +1,30 @@
+-----BEGIN CERTIFICATE-----
+MIIFETCCAvmgAwIBAgIUJHHkhqzO24Jgafn0ISfXwYyJc48wDQYJKoZIhvcNAQEL
+BQAwFzEVMBMGA1UEAwwMcmF1Yy1leGFtcGxlMCAXDTI0MTIxMjA5NTkxNVoYDzIx
+MjQxMjEzMDk1OTE1WjAXMRUwEwYDVQQDDAxyYXVjLWV4YW1wbGUwggIiMA0GCSqG
+SIb3DQEBAQUAA4ICDwAwggIKAoICAQC20YAJLo+km06dzHE+mhT7TwNukByrTH7J
+koPEnLStCLmLnONjOEXi0ji2ejkSwayJ4JVuDc66GcpBZcPGAJSbJDoEpppgYs1u
+Kl/8i71EDuVyo/u2M1Gar0oijvoMriMdhCBKaXuMo/Saummhyh1J9mBKHBQRUP9y
+LFH/FPlp/IpO0idcps14EiIJocxSALzpfDF64UrKoSEv9ZQ8MFWlG1k/8plEnQuz
+xDW8subdZ4iTDCIkmpIVrnGVYWdR12YPlPgY8FMNuEqLKaBlqq84wtPFYMje2KGV
+oFkI9xu+fHPW9Y+/veTqNiM7sYESxP6q6EUykaPWQLg0KNBgl8OCyoOhVXY3Qlfx
+6cMp8rD2sleFW4zqgoOIHpqFBXKCI6JJSFSSu+q8uFDcsDe1xWPLaHHGtyBur1p1
+CbfsAhvI8vmVoVi2YcybjUfYSw+4FBviNN2uCsIIX0SDerm9TvDByYr3UFoFIZOG
+4N8f/9GxL16OPmtirBM+7hyMOpjwKxatGPg6Tg5ByHHwaaUzMMyNCOZyY3LbnMuV
+BWdGKnV0vn/34nfmAks4E7MxgUJ+YAv3LbBGeYOOagi6fqo+r2V+SCCf31+ODtkv
+tb33Gy+BAhhR9tSsPZP97QDr0aTUiZ1Pm+ARFp8aIbBbSwdmsKKEXrcxRiuCGFWn
+xHci9vaVbQIDAQABo1MwUTAdBgNVHQ4EFgQUWNiJ4f9cmP91OsiY41IqMxUKFUUw
+HwYDVR0jBBgwFoAUWNiJ4f9cmP91OsiY41IqMxUKFUUwDwYDVR0TAQH/BAUwAwEB
+/zANBgkqhkiG9w0BAQsFAAOCAgEAdtsvSZKeu/r5yP9WddQ9NrhLYAZyhNwiZMYh
+OBYKMFDI1BjiaXxWfxb8xRZ4b88UJA8PCGX5eHh02+GFfYx7RGNa0A9/MucQS9lw
+27OfTqvIJJ9irNqRP+i1d11MelCK/hrVnLfu8OnRUY5KxIGb9yXCx9zWwxNfzpDE
+ElaAYVE/2e2HV90Tb3gU5esEqmy4XP93u4XEzHV9ZqDUX60Plp4ZV6nyLeNEZfNY
+lODZ8dLIxGShf5xAfgP+yC/YBt7atBZb/qxWcJKheBdPokkmzxUQ+vLklJngnjnl
+m1k2gwRnr5WxcHrZOVqf5cmKxwUG/YNytMti5tKKwGrMnsZHvTsxXW5Z5jBKvqqM
+Z7cWwp0Kld4tG2rxl79skouYHjiAJJKC+vDDl2xPLoPNOlffod9K4Tkep2/PpIye
+cHS7atCmX5MdGQfcKD+diPYAZpEvIhwF4NfWx0NTydMJ6B3ppk2lXk6I4xcmZ9fE
+Gp8VtgbMamiYrBDyJC+6j6O48OnpCdhMu3z48U8Qhghf5OW1lfheEsJ7XWgYj8kB
+swABGHeGuo49AGX+mu3DAfjjNJNsN/sFUfBqzAFEMINOZPIIxdROBAWoVA54ISw+
+JNCH+2bMqqI6I9ZFyzEbXTRW1mzzlFQdXLq+d6cAS1fjYew37MIiPvgjLqRNro0T
+7cSy9tE=
+-----END CERTIFICATE-----

meta-dumpling/dynamic-layers/rauc/recipes-core/rauc/files/example.key.pem
@@ -1,0 +1,52 @@
+-----BEGIN PRIVATE KEY-----
+MIIJQgIBADANBgkqhkiG9w0BAQEFAASCCSwwggkoAgEAAoICAQC20YAJLo+km06d
+zHE+mhT7TwNukByrTH7JkoPEnLStCLmLnONjOEXi0ji2ejkSwayJ4JVuDc66GcpB
+ZcPGAJSbJDoEpppgYs1uKl/8i71EDuVyo/u2M1Gar0oijvoMriMdhCBKaXuMo/Sa
+ummhyh1J9mBKHBQRUP9yLFH/FPlp/IpO0idcps14EiIJocxSALzpfDF64UrKoSEv
+9ZQ8MFWlG1k/8plEnQuzxDW8subdZ4iTDCIkmpIVrnGVYWdR12YPlPgY8FMNuEqL
+KaBlqq84wtPFYMje2KGVoFkI9xu+fHPW9Y+/veTqNiM7sYESxP6q6EUykaPWQLg0
+KNBgl8OCyoOhVXY3Qlfx6cMp8rD2sleFW4zqgoOIHpqFBXKCI6JJSFSSu+q8uFDc
+sDe1xWPLaHHGtyBur1p1CbfsAhvI8vmVoVi2YcybjUfYSw+4FBviNN2uCsIIX0SD
+erm9TvDByYr3UFoFIZOG4N8f/9GxL16OPmtirBM+7hyMOpjwKxatGPg6Tg5ByHHw
+aaUzMMyNCOZyY3LbnMuVBWdGKnV0vn/34nfmAks4E7MxgUJ+YAv3LbBGeYOOagi6
+fqo+r2V+SCCf31+ODtkvtb33Gy+BAhhR9tSsPZP97QDr0aTUiZ1Pm+ARFp8aIbBb
+SwdmsKKEXrcxRiuCGFWnxHci9vaVbQIDAQABAoICAFilxJpvL+rm/huJL15mM4da
+nHTnv2cWvCH9ABT90aRX5zS6Gg6aElU2j5Z4NKWSMRpYQs0vN89Iwyir8l7YGDcU
+PWfU62WScz+Yn5ujnN5cBMlul5cVUlwZ5G5WewxcAh4FW+0JMgYT2H+Qpbj0evhK
+1g0JiXeOGZegww/X+Ur50I0A3WJRZa9zx3p5MMyQnAnxlUKa7PvNXvzU7ZCz9UuO
+s9vMxKIfLrX4DgCWlVjFo9V93C54nx6RTHVdWk62jkA/AYCCRBuSgd7yC9tQhQ+W
+dT6vvCdGzy9xJcsxm4vl5/vTbeVRbKdfIf2HiGl0CXMRFsC7G0FAd7GfGs/Dt2Kp
+7ESBi2x5k9LluDhFNSirkVi9MNcvligRm3Y4TPUkIs71CW+VzBxj+SW0y+lhZfne
+ts215dJqNTJWaoGNQ0B4fIQW6pnYk+RcjX0HCzp9YFdE+M5eM3GqwFLy6bXSyfTJ
+Xss+xQAm4eadR/awPIxErRVvRJzj2/6mDa1nPDZfN+ZAmMjLDk7L9vISXxy8lWTK
+nfcQlZM2B+2UliILouNiYT4rdYKdi2WV6hisOKPcflehfuO7ylu5k5KxnftDPhnD
+Pc3j4YAQxgofWFoCRuVyIz6SmlrtOXjh700SHLHX2l1Zf1Qis5D1jlGfY57qX+yc
+NpjnNXQoVNc7zg0G5r/3AoIBAQDfWeEGBJEg28CViDEVE/D/A7GL+i8g8En70O27
+nEDe4vtI5Vjw0OB+diAQlAMsnXqZAUSTeiFvbXf0ZuT3Oc8XQ1TRpKQOXcBw1SNQ
+DOORO2PYBLfFRRmH7nlLlbR/wfCbyhbkn/MR3dHhlz1f+yRFxVOfpMuUh0wyNuyA
+msZ/M64PmALzfbxHtuW4oQa+GO3+Trun1w73u6uCDxv+cVybIXHCOjRekbppTdL8
+zOCfAkc0pVyldDpBnNhnlmMN3CgYG2yS6iVtVqENyCVRs+SiitHag+H5yT1oy7sK
+rUReodHh7H7MUIgogiuw8BL3Ya3lG6jqh7wlndgrYk8dpT/LAoIBAQDRitPmQJv+
+HCDBAqiN6g1slOUz8oMNRNSp6RgroWO1z2+C0RYOXntTm8BbnqMfiWAt3iLC7pA9
+veHWeINZSoBsfXvN4ZrLB+2NG+39qdJeGH7/H0Ua5QrsBfriZGoFr5eEWSI1KcbB
+zl6oea6jkOVxiWP1pQhcOAwgFn6bIVl3gYaaYA5VympYPYx/28sSjRJIcVSmR6Nz
+ZrtzOiTVNCBoxcpTCw8OyBEVYhDOEgnODedRNGgiGGU3bEFttTMeaPC9FpZyjiHc
+5GY6rAWWrQoIMtTGG4FJeu027nDWWv3jESKkGECVn2Rx+3xOpJcleLyhi9z+TH15
+5+EOwvt7zuinAoIBAQCAkh6k9jAM+DNOYM3QUmBijF/CMky661fLa6POYc49/++j
+MNMYHUV9xsYChRGmtyiBt2cgdieVGzD4MdsQLej2fT3wSJRf6LwEt8zSIflF+ygo
+i9614mcmBrMwPYdgG+ZD5/txtFmaKVTORd+AxvNEbqkKtzSx1YiZR2CxT2eAheFm
+/JyGShT+O1zWAWv+Nh+aSvxfS79EAHZkMgptWBh0/LWdlx/aaI0kXTZdbBrJregL
+eD1yyQtYBc/VIY73xYoXBoJjuAG/hSTE6DgDZkjxAHR5E34b6io9jzmmlcNLrl9B
+bIA5YQpgNz6rZiek3EfcISYFjAlUph5wKvFcIWotAoIBAD+4ILG+6fC+6vLmdQ0a
+pFRlfsFqOJHbYXP9rtztqd2Ko9U68aMS9hu5nCgAOIiJhNbN2vI+C1zLFhba9M49
+SoozKmez+BdPZNxpJtjhQFheyJeaEbCY/9ZAB0e600bBW6jiVfWmq4sZz00aG5If
+qLX62ovDUKSCYqWLUZqo7ITbvWvKU/GgqPAGX2guFTWh8nJXDC+4TyrzjYL/pl2z
+CTvzEZBpJ34ONquy83tGnvaq4bGe5x+fHYBaChdrZ40SykolyUAymneKeM/Uqapl
+Gkzd8DgB+zG2ENaIcYWti9D1z/voaBNXsfq7R9S+VjwN2Aaj+mLWLb3ZVmXfwwum
+IrcCggEANb++yV2dspk5UjLIgCdvzrQeX7qZw3XCjT5f25Z9bHY+8yfnKGj6i8rV
+takaIFTTMpXJ2wALi0adhJUwdHFVyVaG/e3riX4qNFN0mYVAQOsrqoM54WHj9T98
+ahmPJ7BiF4Lfmb0VZg3UH2geUP+0rgq6rDSwct/eVfC+gh6zfehP3jHnj+r/NuoB
+seihKfQeFIsa/0Kl2Akcj+A1BP7NbWEP7E8km9+oCgBXPbIKT7/Wr/K6tuTZtNBv
+d+o5PdLWgUvnyEzv29SVo+ItinpiI5CRu0yRyCbsvFRzgMa1468Rb9alKsIWZyWR
+/AT+R1QirSxJqyHwghAo8L34W1RlYA==
+-----END PRIVATE KEY-----

meta-dumpling/dynamic-layers/rauc/recipes-core/rauc/files/rauc-conf.sh
@@ -1,0 +1,47 @@
+#!/bin/sh
+
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2024 TQ-Systems GmbH <oss@ew.tq-group.com>, D-82229 Seefeld, Germany.
+# Author: Matthias Schiffer
+
+set -e
+
+RUNTIME_CONFIG=/run/tq/rauc/system.conf
+
+get_root_dev () {
+	findmnt --evaluate --first-only --noheadings --output SOURCE --mountpoint /
+}
+
+get_config_suffix () {
+	case "$(get_root_dev)" in
+	/dev/mmcblk0p*)
+		echo mmc0
+		;;
+	/dev/mmcblk1p*)
+		echo mmc1
+		;;
+	ubi0:*)
+		echo spi
+		;;
+	esac
+}
+
+get_system_config () {
+	echo "/etc/rauc/system.conf-$(get_config_suffix)"
+}
+
+main () {
+	local config
+
+	config=$(get_system_config)
+
+	if ! [ -r "$config" ]; then
+		config=/dev/null
+	fi
+
+	mkdir -p "$(dirname "$RUNTIME_CONFIG")"
+	ln -sfn "$config" "$RUNTIME_CONFIG"
+}
+
+main "$@"

meta-dumpling/dynamic-layers/rauc/recipes-core/rauc/files/system.conf-mmc0
@@ -1,0 +1,22 @@
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2024 TQ-Systems GmbH <oss@ew.tq-group.com>, D-82229 Seefeld, Germany.
+# Author: Matthias Schiffer
+
+[system]
+compatible=@RAUC_BUNDLE_COMPATIBLE@
+bootloader=uboot
+bundle-formats=-plain
+
+[keyring]
+path=@RAUC_KEYRING_FILE@
+
+[slot.rootfs.0]
+device=/dev/mmcblk0p2
+type=ext4
+bootname=A
+
+[slot.rootfs.1]
+device=/dev/mmcblk0p3
+type=ext4
+bootname=B

commit b5e5d7837bc05a0f2e70e38f1d8a015d1dcd113d

meta-dumpling: rauc: move assignment for RAUC_*_FILE to recipe
When assignment is done inside the distro config the recipes
will fail to resolve SRC_URI if using a different distro. Assign
the variables in a shared include to be used rauc related recipes
in meta-dumpling.

Fixes: 4c8e7d1ac1ab ("meta-dumpling: add example RAUC configuration")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

meta-dumpling/conf/distro/include/tq-common.inc
@@ -14,11 +14,6 @@ RAUC_BUNDLE_VERSION ?= "r0"
 
 SKIP_RECIPE[rauc-conf] = "replaced with tq-rauc-conf"
 
-# RAUC example key - located under (and used by recipes in) dynamic-layers/rauc
-RAUC_KEYRING_FILE ?= "example.cert.pem"
-RAUC_CERT_FILE ?= "example.cert.pem"
-RAUC_KEY_FILE ?= "example.key.pem"
-
 # RAUC upgrades are possible between images that use the same
 # `RAUC_BUNDLE_COMPATIBLE`. The reference distros in meta-dumpling always use
 # "dumpling-${MACHINE}", so upgrades between dumpling and spaetzle are supported.

meta-dumpling/dynamic-layers/rauc/recipes-core/rauc/example-files.inc
@@ -1,0 +1,4 @@
+# RAUC example key - located under (and used by recipes in) dynamic-layers/rauc
+RAUC_KEYRING_FILE ?= "example.cert.pem"
+RAUC_CERT_FILE ?= "example.cert.pem"
+RAUC_KEY_FILE ?= "example.key.pem"

meta-dumpling/dynamic-layers/rauc/recipes-core/rauc/tq-bundle.bb
@@ -3,6 +3,8 @@ inherit bundle
 LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://${COMMON_LICENSE_DIR}/MIT;md5=0835ade698e0bcf8506ecda2f7b4f302"
 
+require example-files.inc
+
 RAUC_KEY_URI ??= "file://${RAUC_KEY_FILE}"
 RAUC_CERT_URI ??= "file://${RAUC_CERT_FILE}"
 RAUC_KEYRING_URI ??= "file://${RAUC_KEYRING_FILE}"

meta-dumpling/dynamic-layers/rauc/recipes-core/rauc/tq-rauc-conf.bb
@@ -9,6 +9,8 @@ SUMMARY = "RAUC system configuration & verification keyring (TQ example config)"
 LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://${COREBASE}/meta/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
 
+require example-files.inc
+
 RAUC_KEYRING_URI ??= "file://${RAUC_KEYRING_FILE}"
 
 RDEPENDS:${PN} += "util-linux-findmnt"