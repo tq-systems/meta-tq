tq-systems/meta-tq

commit 88d1fe2eda132bc5c3df6186e0ca46836b00e688

u-boot-tq_2026.01: update to 5c778312688bd46835a93b811c51f1510e0b3cb9
5c778312688 [HACK] board: tqma64xxl: work around Nanya RAM issue
a66ba593528 arm: dts: k3-am64: update Nanya DTS for TQMa64xxL
b766d681c1d sysinfo: tq_eeprom: add SYSID_TQ_RAM_TYPE
15407d39d4c arm: mach-k3: am64x: call spl_board_init_f
54a48f0b47d arm: spl.c: add spl_board_init_f
5ed2352978d sysinfo: move helpers inside include guard
e02c04473ae config: tqma[62,64,67]xx: enable cmd_hash
0b5197f4cf3 config: tqma[62,64,67]xx: enable CMD_DDR4
a8dd257e4c5 config: tqma[62,64,67]xx: remove nonexisting CONFIG_CMD_DDRSS

Signed-off-by: Alexander Feilke <alexander.feilke@ew.tq-group.com>

meta-tq/recipes-bsp/u-boot/u-boot-tq_2026.01.bb
@@ -22,7 +22,7 @@ require recipes-bsp/u-boot/u-boot-tq.inc
 UBOOT_INITIAL_ENV = "u-boot-initial-env"
 
 SRCBRANCH = "TQMaxx-u-boot-v2026.01"
-SRCREV = "e02c04473aefff6a70cfee46b3dfa91652d5e6d7"
+SRCREV = "5c778312688bd46835a93b811c51f1510e0b3cb9 "
 
 DEPENDS += "python3-setuptools-native"
 

commit 45ff1e0d3d540000e4b9d8f425745eb4417765f1

meta-tq: u-boot-tq_2026.01: fix typo for SRCREV
Fixes: 88d1fe2eda13 ("u-boot-tq_2026.01: update to 5c778312688bd46835a93b811c51f1510e0b3cb9")

Signed-off-by: Markus Niebel <Markus.Niebel@ew.tq-group.com>

meta-tq/recipes-bsp/u-boot/u-boot-tq_2026.01.bb
@@ -22,7 +22,7 @@ require recipes-bsp/u-boot/u-boot-tq.inc
 UBOOT_INITIAL_ENV = "u-boot-initial-env"
 
 SRCBRANCH = "TQMaxx-u-boot-v2026.01"
-SRCREV = "5c778312688bd46835a93b811c51f1510e0b3cb9 "
+SRCREV = "5c778312688bd46835a93b811c51f1510e0b3cb9"
 
 DEPENDS += "python3-setuptools-native"
 